6c9da01683724c40e67d0dff6f58408915df67fe
Andre Luiz <adventureandre@hotmail.com>
2024-02-21 13:01:58 -0300

    Update config-pix.php

--- config-pix.php
+++ config-pix.php
@@ -10,6 +10,6 @@ define('PIX_MERCHANT_CITY','SAO PAULO');
 
 //DADOS DA API PIX (DINÂMICO)
 define('API_PIX_URL','https://api-pix.gerencianet.com.br');
-define('API_PIX_CLIENT_ID','Client_Id_bceaadc520b2c9ee2a201980a9cf64433e0b4c49');
-define('API_PIX_CLIENT_SECRET','Client_Secret_6d3e132c9a01f164893e4eb6a78253f1ea97fc7a');
+define('API_PIX_CLIENT_ID','Client_Id');
+define('API_PIX_CLIENT_SECRET','Client_Secret');
 define('API_PIX_CERTIFICATE',__DIR__.'/files/certificates/gerencianet.pem');
